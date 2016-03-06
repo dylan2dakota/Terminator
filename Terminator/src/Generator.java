6e258755e59154b3b506c70c62293b09cb435bdd
Dakodah Davis <d.sdavis1031@gmail.com>
2016-03-05 20:48:12 -0500

    Added arrays for points and for loop to populate them

--- Terminator/src/Generator.java
+++ Terminator/src/Generator.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 
 /**
  * @author Owner
@@ -7,9 +7,21 @@
  */
 public class Generator {
 
-	private double[] points;
+	private double[] x;
+	private double[] y;
 
 	public Generator(){
+		int i,j;
+		//random object for generator
+		Random rn = new Random();
+		
+		//generate random points
+		for(i=0;i<100;i++){
+			x[i] = (rn.nextDouble()*999)+1;
+		}
+		for(j=0;j<100;j++){
+			y[j] = (rn.nextDouble()*700);
+		}		
 
 	}
 
