7c5c81ff22679a58f0e1eaf664818fc916b08342
coxd6 <coxd6>
2016-03-07 17:35:21 -0500

    ONE SOURCE

--- Terminator/src/NavigationSimulation.java
+++ Terminator/src/NavigationSimulation.java
@@ -1,4 +1,4 @@
-package src;
+
 import javafx.scene.control.Menu;
 import javafx.scene.control.Button;
 
--- Terminator/src/Robot.java
+++ Terminator/src/Robot.java
@@ -1,4 +1,4 @@
-package src;
+
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
