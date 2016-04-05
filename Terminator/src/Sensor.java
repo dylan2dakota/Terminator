69c7c48d30b67f4aed4ceaa2616ab63f8e7e1a47
watsonr9 <watsonr9@DB-LIB-41.erau.edu>
2016-04-05 17:08:50 -0400

    Worked on sensor class. Finding closest point.

--- Terminator/src/Sensor.java
+++ Terminator/src/Sensor.java
@@ -6,6 +6,13 @@ import javafx.scene.shape.ArcType;
 
 public class Sensor extends Arc {
 
+	SetupPage robotInfo = new SetupPage();
+	Point[] points = robotInfo.generatedPoints;
+	int sensorAngle = robotInfo.sensorAngle;
+	int sensorRange = robotInfo.sensorRange;
+	int robotX;
+	int robotY;
+	
 	public Sensor() {
 
 	}
@@ -24,4 +31,35 @@ public class Sensor extends Arc {
 		setStrokeWidth(5);
 
 	}
+	
+	public int[] pointDistance(Point[] points, int robotX, int robotY){
+		
+		int[] nearestLocation = {0,0};
+		double nearestDistance = 0;
+		
+		int i;
+		while(i < points.length){
+			Point interestPoint =points[i];
+			double xDist = Math.abs(interestPoint.getCenterX()-robotX);
+			double yDist = Math.abs(interestPoint.getCenterY()-robotY);
+			
+			double straightDistance = Math.sqrt( (Math.pow(xDist,2)+Math.pow(yDist,2)) );
+			
+			if(straightDistance<nearestDistance){
+				nearestDistance = straightDistance;
+				nearestLocation = {xDist,yDist};
+			}
+			i++;
+		}
+		
+		return nearestLocation;
+	}//end pointDistance
+	
+	public Boolean pointSensed(int sensorAngle, int sensorRange){ //determines if nearest point is within sensor area
+		
+		Boolean sensed = false;
+		
+		return sensed;
+	}
+	
 }// end Sensor
